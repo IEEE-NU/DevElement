79b9d17a1be7cb5abf21b752f8475e4542ee1283
Jacob Bruce <jacobbruce2016@u.northwestern.edu>
2014-02-22 13:51:08 -0600

    Added limited search functionality
    Search page now interfaces with database, but only in a limited fashion.

--- controllers/search.js
+++ controllers/search.js
@@ -1,10 +1,32 @@
+var mongoose = require('mongoose');
+var passport = require('passport');
+var _ = require('underscore');
+var User = require('../models/User');
+var Group = require('../models/Group');
+var Group = mongoose.model('Group');
+var daysOfWeek = ["Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"];
+
 exports.search = function(req,res){
-	res.render('search',{
-		title: 'Search'
+	var results = Group.findOne({ 'course': 'EECS 211'});
+	results.select('course meetingLocation meetingTime meetingDate');
+	
+
+	results.exec(function(err,group){
+		res.render('search',{
+		title: 'Search',
+		groups: group
+	});
 	});
 };
 
 exports.submitSearch = function(req,res){
-	req.params
+	var results = Group.findOne({ 'course': 'EECS 211'});
+	results.select('course meetingLocation meetingTime meetingDate');
 
+	results.exec(function(err,group) {
+		res.render('search', {
+		title: 'Search',
+		groups: group
+	});
+	});
 };
